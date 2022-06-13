dbf7bfc752102347222fed8252d8a980aff16f46
Andrew Yang <andy.tubeee@gmail.com>
2022-06-13 09:25:54 -0400

    riemann sum

--- frontend/components/Function/RiemannSum.js
+++ frontend/components/Function/RiemannSum.js
@@ -1,11 +1,47 @@
 import React, { useEffect, useState } from 'react';
+import { simplify, parse, derivative, isInteger } from 'mathjs';
 import Swal from 'sweetalert2';
 
 export default function RiemannSum({ func }) {
-    const [param, setParam] = useState({});
-    const ComputeRS = () => {
-        
+  const [params, setParam] = useState({});
+  const [res, setRes] = useState();
+  const ComputeRS = (type) => {
+    let sum = 0;
+    const dx = (params.end - params.start) / params.subdivisons;
+    const f = simplify(parse(func));
+    switch (type) {
+      case 'LEFT':
+        for (let i = 0; i < params.subdivisons; i++) {
+          const res = f.evaluate({ x: params.start + i * dx });
+          sum += res * dx;
+        }
+        break;
+      case 'RIGHT':
+        for (let i = 1; i <= params.subdivisons; i++) {
+          const res = f.evaluate({ x: params.start + i * dx });
+          sum += res * dx;
+        }
+        break;
+      case 'MIDPOINT':
+        for (let i = 0; i < params.subdivisons; i++) {
+          const x = params.start + i * dx + (params.start + (i + 1) * dx);
+          const res = f.evaluate({ x: x / 2 });
+          sum += res * dx;
+        }
+        break;
+      case 'TRAPEZOIDAL':
+        for (let i = 0; i <= params.subdivisons; i++) {
+          if (i == 0 || i == params.subdivisons) {
+            sum += f.evaluate({ x: params.start + i * dx });
+          } else {
+            sum += f.evaluate({ x: params.start + i * dx }) * 2;
+          }
+        }
+        sum *= dx / 2;
+        break;
     }
+    return sum;
+  };
   return (
     <div className='flex flex-col gap-3'>
       <h1 className='font-bold'>Riemann Sum Integral</h1>
@@ -15,7 +51,7 @@ export default function RiemannSum({ func }) {
           placeholder='Starting'
           className='w-1/3 rounded border pl-2'
           onChange={(e) => {
-            setParam({ ...param, start: +e.target.value });
+            setParam({ ...params, start: +e.target.value });
           }}
         />
         <input
@@ -23,7 +59,7 @@ export default function RiemannSum({ func }) {
           type='number'
           className='w-1/3 rounded border pl-2'
           onChange={(e) => {
-            setParam({ ...param, end: +e.target.value });
+            setParam({ ...params, end: +e.target.value });
           }}
         />
         <input
@@ -31,24 +67,57 @@ export default function RiemannSum({ func }) {
           type='number'
           className='w-1/3 rounded border pl-2'
           onChange={(e) => {
-            setParam({ ...param, dx: +e.target.value });
+            setParam({ ...params, subdivisons: +e.target.value });
           }}
         />
       </div>
       <div class='flex gap-2'>
-        <button type='number' className={`btn-cyan`} onClick={() => {}}>
+        <button
+          type='number'
+          className={`btn-cyan`}
+          onClick={() => {
+            setParam({ ...params, mode: 'LEFT' });
+            setRes(ComputeRS('LEFT'));
+          }}
+        >
           Left
         </button>
-        <button type='number' className={`btn-cyan`} onClick={() => {}}>
+        <button
+          type='number'
+          className={`btn-cyan`}
+          onClick={() => {
+            setParam({ ...params, mode: 'RIGHT' });
+            setRes(ComputeRS('RIGHT'));
+          }}
+        >
           Right
         </button>
-        <button type='number' className={`btn-cyan`} onClick={() => {}}>
-          Middle
+        <button
+          type='number'
+          className={`btn-cyan`}
+          onClick={() => {
+            setParam({ ...params, mode: 'MIDPOINT' });
+            setRes(ComputeRS('MIDPOINT'));
+          }}
+        >
+          Midpoint
         </button>
-        <button type='number' className={`btn-cyan`} onClick={() => {}}>
+        <button
+          type='number'
+          className={`btn-cyan`}
+          onClick={() => {
+            setParam({ ...params, mode: 'TRAPEZOIDAL' });
+            setRes(ComputeRS('TRAPEZOIDAL'));
+          }}
+        >
           Trapezoidal
         </button>
       </div>
+      {res && (
+        <h1>
+          {params.mode}: {res}
+        </h1>
+      )}
     </div>
   );
 }
